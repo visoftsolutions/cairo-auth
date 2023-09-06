965c5c8861731c46e0ab464f4644d2308bd2c175
Paweł <neotheprogramist@proton.me>
2023-09-06 23:05:10 +0200

    create proxy server in progress

---
+++ server/src/api/mod.rs
@@ -0,0 +1,5 @@
+pub mod square;
+
+pub async fn root() -> &'static str {
+    "Hello, World!"
+}
---
+++ server/src/main.rs
@@ -0,0 +1,27 @@
+mod api;
+mod shutdown_signal;
+
+use axum::{
+    routing::{get, post},
+    Router,
+};
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+
+    let addr = std::env::var("ADDRESS").unwrap().parse().unwrap();
+
+    let app = Router::new()
+        .route("/", get(api::root))
+        .route("/square", post(api::square::root));
+
+    tracing::info!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .with_graceful_shutdown(async {
+            shutdown_signal::listen().await;
+        })
+        .await
+        .unwrap();
+}
